ef31d641a4f483cb4b95a2c220b8eab62a746bf3
Darren Ehlers <me@darrenehlers.com>
2020-01-03 09:36:42 -0600

    Added NFCTags Protocol

---
+++ Sources/DNSProtocols/Worker Protocols/PTCLNFCTags_Protocol.swift
@@ -0,0 +1,87 @@
+//
+//  PTCLNFCTags_Protocol.swift
+//  DoubleNode Swift Framework (DNSFramework) - DNSProtocols
+//
+//  Created by Darren Ehlers on 2019/08/12.
+//  Copyright © 2019 - 2016 Darren Ehlers and DoubleNode, LLC. All rights reserved.
+//
+
+import CoreNFC
+import Foundation
+
+public protocol DNSError: LocalizedError {
+    var nsError: NSError! { get }
+}
+
+public enum PTCLNFCTagsError: Error
+{
+    case notSupported(domain: String, file: String, line: String, method: String)
+    case system(domain: String, file: String, line: String, method: String)
+    case timeout(domain: String, file: String, line: String, method: String)
+}
+
+extension PTCLNFCTagsError: DNSError {
+    public var nsError: NSError! {
+        switch self {
+        case .notSupported(let domain, let file, let line, let method):
+            let userInfo: [String : Any] = ["DNSDomain": domain,
+                                            "DNSFile": file,
+                                            "DNSLine": line,
+                                            "DNSMethod": method,
+                                            NSLocalizedDescriptionKey: self.errorDescription ?? "Unknown Error"]
+            return NSError.init(domain: domain, code: -9999, userInfo: userInfo)
+
+        case .system(let domain, let file, let line, let method):
+            let userInfo: [String : Any] = ["DNSDomain": domain,
+                                            "DNSFile": file,
+                                            "DNSLine": line,
+                                            "DNSMethod": method,
+                                            NSLocalizedDescriptionKey: self.errorDescription ?? "Unknown Error"]
+            return NSError.init(domain: domain, code: -9999, userInfo: userInfo)
+
+        case .timeout(let domain, let file, let line, let method):
+            let userInfo: [String : Any] = ["DNSDomain": domain,
+                                            "DNSFile": file,
+                                            "DNSLine": line,
+                                            "DNSMethod": method,
+                                            NSLocalizedDescriptionKey: self.errorDescription ?? "Unknown Error"]
+            return NSError.init(domain: domain, code: -9999, userInfo: userInfo)
+        }
+    }
+    public var errorDescription: String? {
+        switch self {
+        case .notSupported(let domain, let file, let line, let method):
+            return NSLocalizedString(
+                "Your system doesn't support NFC Tags (\(domain):\(file):\(line):\(method))",
+                comment: ""
+            )
+
+        case .system(let domain, let file, let line, let method):
+            return NSLocalizedString(
+                "CoreNFC System Error (\(domain):\(file):\(line):\(method))",
+                comment: ""
+            )
+
+        case .timeout(let domain, let file, let line, let method):
+            return NSLocalizedString(
+                "NFC Reader Timeout Error (\(domain):\(file):\(line):\(method))",
+                comment: ""
+            )
+        }
+    }
+}
+
+// (object: Any?, error: Error?)
+public typealias PTCLNFCTagsBlockVoidArrayNFCNDEFMessageError = ([NFCNDEFMessage], DNSError?) -> Void
+
+public protocol PTCLNFCTags_Protocol: PTCLBase_Protocol {
+    var nextWorker: PTCLNFCTags_Protocol? { get }
+
+    init()
+    init(nextWorker: PTCLNFCTags_Protocol)
+
+    // MARK: - Business Logic / Single Item CRUD
+    func doScanTags(for key: String,
+                    with progress: PTCLProgressBlock?,
+                    and block: PTCLNFCTagsBlockVoidArrayNFCNDEFMessageError?) throws
+}
