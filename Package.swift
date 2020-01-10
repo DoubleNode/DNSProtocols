354d422a2c90dc2617ceb438aed244c71ec215d5
Darren Ehlers <me@darrenehlers.com>
2020-01-10 10:28:55 -0600

    Added Beacons Protocol

--- Package.swift
+++ Package.swift
@@ -25,14 +25,14 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/DoubleNode/DNSDataObjects.git", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "DNSProtocols",
-            dependencies: []),
+            dependencies: ["DNSDataObjects"]),
         .testTarget(
             name: "DNSProtocolsTests",
             dependencies: ["DNSProtocols"]),
---
+++ Sources/DNSProtocols/Worker Protocols/PTCLBeacons_Protocol.swift
@@ -0,0 +1,27 @@
+//
+//  PTCLBeacons_Protocol.swift
+//  DoubleNode Swift Framework (DNSFramework) - DNSProtocols
+//
+//  Created by Darren Ehlers on 2019/08/12.
+//  Copyright © 2019 - 2016 Darren Ehlers and DoubleNode, LLC. All rights reserved.
+//
+
+import DNSDataObjects
+import Foundation
+
+// (beacons: [DAOBeacon], error: Error?)
+public typealias PTCLBeaconsBlockVoidArrayDAOBeaconError = ([DAOBeacon], DNSError?) -> Void
+
+public protocol PTCLBeacons_Protocol: PTCLBase_Protocol {
+    var nextWorker: PTCLBeacons_Protocol? { get }
+
+    init()
+    init(nextWorker: PTCLBeacons_Protocol)
+
+    // MARK: - Business Logic / Single Item CRUD
+
+    func doRangeBeacons(for keys: [UUID],
+                        with progress: PTCLProgressBlock?,
+                        and block: PTCLBeaconsBlockVoidArrayDAOBeaconError?) throws
+    func doStopRangeBeacons() throws
+}
