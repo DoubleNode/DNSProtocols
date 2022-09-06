0547b251112db6e3696be72b869b99a1e24d5fe1
Darren Ehlers <me@darrenehlers.com>
2022-09-06 13:32:23 -0500

    Update package dependencies

--- Package.swift
+++ Package.swift
@@ -27,9 +27,9 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.6.1"),
-        .package(url: "https://github.com/DoubleNode/DNSCore.git", from: "1.9.28"),
+        .package(url: "https://github.com/DoubleNode/DNSCore.git", from: "1.9.32"),
         .package(url: "https://github.com/DoubleNode/DNSCoreThreading.git", from: "1.9.0"),
-        .package(url: "https://github.com/DoubleNode/DNSDataObjects.git", from: "1.9.36"),
+        .package(url: "https://github.com/DoubleNode/DNSDataObjects.git", from: "1.9.39"),
         .package(url: "https://github.com/DoubleNode/DNSError.git", from: "1.9.2"),
     ],
     targets: [
