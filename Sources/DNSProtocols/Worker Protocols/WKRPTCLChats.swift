ddd1ff7c9a9f1965c33512ebfe80b8670759d683
Darren Ehlers <me@darrenehlers.com>
2022-12-03 20:28:19 -0600

    Added Media and Chats protocols

---
+++ Sources/DNSProtocols/Worker Protocols/WKRPTCLChats.swift
@@ -0,0 +1,60 @@
+//
+//  WKRPTCLChats.swift
+//  DoubleNode Swift Framework (DNSFramework) - DNSProtocols
+//
+//  Created by Darren Ehlers.
+//  Copyright © 2022 - 2016 DoubleNode.com. All rights reserved.
+//
+
+import Combine
+import DNSDataObjects
+import Foundation
+
+// Protocol Return Types
+public typealias WKRPTCLChatsRtnChat = DAOChat
+public typealias WKRPTCLChatsRtnAChatMessage = [DAOChatMessage]
+public typealias WKRPTCLChatsRtnVoid = Void
+
+// Protocol Result Types
+public typealias WKRPTCLChatsResChat = Result<WKRPTCLChatsRtnChat, Error>
+public typealias WKRPTCLChatsResAChatMessage = Result<WKRPTCLChatsRtnAChatMessage, Error>
+public typealias WKRPTCLChatsResVoid = Result<WKRPTCLChatsRtnVoid, Error>
+
+// Protocol Block Types
+public typealias WKRPTCLChatsBlkChat = (WKRPTCLChatsResChat) -> Void
+public typealias WKRPTCLChatsBlkAChatMessage = (WKRPTCLChatsResAChatMessage) -> Void
+public typealias WKRPTCLChatsBlkVoid = (WKRPTCLChatsResVoid) -> Void
+
+public protocol WKRPTCLChats: WKRPTCLWorkerBase {
+    var callNextWhen: DNSPTCLWorker.Call.NextWhen { get }
+    var nextWorker: WKRPTCLChats? { get }
+    var wkrSystems: WKRPTCLSystems? { get }
+
+    init()
+    func register(nextWorker: WKRPTCLChats,
+                  for callNextWhen: DNSPTCLWorker.Call.NextWhen)
+
+    // MARK: - Worker Logic (Public) -
+    func doLoadChat(for id: String,
+                    with progress: DNSPTCLProgressBlock?,
+                    and block: WKRPTCLChatsBlkChat?)
+    func doLoadMessages(for chat: DAOChat,
+                        with progress: DNSPTCLProgressBlock?,
+                        and block: WKRPTCLChatsBlkAChatMessage?)
+    func doRemove(_ message: DAOChatMessage,
+                  with progress: DNSPTCLProgressBlock?,
+                  and block: WKRPTCLChatsBlkVoid?)
+    func doUpdate(_ message: DAOChatMessage,
+                  with progress: DNSPTCLProgressBlock?,
+                  and block: WKRPTCLChatsBlkVoid?)
+
+    // MARK: - Worker Logic (Shortcuts) -
+    func doLoadChat(for id: String,
+                    with block: WKRPTCLChatsBlkChat?)
+    func doLoadMessages(for chat: DAOChat,
+                        with block: WKRPTCLChatsBlkAChatMessage?)
+    func doRemove(_ message: DAOChatMessage,
+                  with block: WKRPTCLChatsBlkVoid?)
+    func doUpdate(_ message: DAOChatMessage,
+                  with block: WKRPTCLChatsBlkVoid?)
+}
