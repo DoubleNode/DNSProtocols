40c2c36d37e5b587607a9f688f1c24465a8571e6
Darren Ehlers <me@darrenehlers.com>
2023-01-06 20:20:26 -0600

    Updated package dependencies

--- Package.swift
+++ Package.swift
@@ -27,9 +27,9 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.6.4"),
-        .package(url: "https://github.com/DoubleNode/DNSCore.git", from: "1.10.18"),
+        .package(url: "https://github.com/DoubleNode/DNSCore.git", from: "1.10.30"),
         .package(url: "https://github.com/DoubleNode/DNSCoreThreading.git", from: "1.10.1"),
-        .package(url: "https://github.com/DoubleNode/DNSDataObjects.git", from: "1.10.55"),
+        .package(url: "https://github.com/DoubleNode/DNSDataObjects.git", from: "1.10.60"),
         .package(url: "https://github.com/DoubleNode/DNSError.git", from: "1.10.0"),
     ],
     targets: [
--- README.md
+++ README.md
@@ -34,16 +34,16 @@ Once you have your Swift package set up, adding Alamofire as a dependency is as 
 
 ```swift
 dependencies: [
-.package(url: "https://github.com/DoubleNode/DNSProtocols.git", from: "1.10.49")
+.package(url: "https://github.com/DoubleNode/DNSProtocols.git", from: "1.10.54")
 ]
 ```
 
 ## Dependencies
 
 -   Alamofire v5.6.4+
--   DNSCore v1.10.18+
+-   DNSCore v1.10.30+
 -   DNSCoreThreading v1.10.1+
--   DNSDataObjects v1.10.55+
+-   DNSDataObjects v1.10.60+
 -   DNSError v1.10.0+
 
 ## Credits
