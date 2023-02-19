3b7d8c90a504ab2d687f2e786b3666ba0b5c9e03
Darren Ehlers <me@darrenehlers.com>
2023-02-19 14:00:56 -0600

    Updated GEO protocol

--- Sources/DNSProtocols/Worker Protocols/WKRPTCLGeo.swift
+++ Sources/DNSProtocols/Worker Protocols/WKRPTCLGeo.swift
@@ -6,18 +6,20 @@
 //  Copyright © 2022 - 2016 DoubleNode.com. All rights reserved.
 //
 
+import CoreLocation
+import DNSCore
 import Foundation
 
 // Protocol Return Types
-public typealias WKRPTCLGeoRtnString = String
+public typealias WKRPTCLGeoRtnStringLocation = (String, CLLocation)
 public typealias WKRPTCLGeoRtnVoid = Void
 
 // Protocol Result Types
-public typealias WKRPTCLGeoResString = Result<WKRPTCLGeoRtnString, Error>
+public typealias WKRPTCLGeoResStringLocation = Result<WKRPTCLGeoRtnStringLocation, Error>
 public typealias WKRPTCLGeoResVoid = Result<WKRPTCLGeoRtnVoid, Error>
 
 // Protocol Block Types
-public typealias WKRPTCLGeoBlkString = (WKRPTCLGeoResString) -> Void
+public typealias WKRPTCLGeoBlkStringLocation = (WKRPTCLGeoResStringLocation) -> Void
 
 public protocol WKRPTCLGeo: WKRPTCLWorkerBase {
     var callNextWhen: DNSPTCLWorker.Call.NextWhen { get }
@@ -30,14 +32,19 @@ public protocol WKRPTCLGeo: WKRPTCLWorkerBase {
 
     // MARK: - Worker Logic (Public) -
     func doLocate(with progress: DNSPTCLProgressBlock?,
-                  and block: WKRPTCLGeoBlkString?)
+                  and block: WKRPTCLGeoBlkStringLocation?)
+    func doLocate(_ address: DNSPostalAddress,
+                  with progress: DNSPTCLProgressBlock?,
+                  and block: WKRPTCLGeoBlkStringLocation?)
     func doStopTrackLocation(for processKey: String) -> WKRPTCLGeoResVoid
     func doTrackLocation(for processKey: String,
                          with progress: DNSPTCLProgressBlock?,
-                         and block: WKRPTCLGeoBlkString?)
+                         and block: WKRPTCLGeoBlkStringLocation?)
 
     // MARK: - Worker Logic (Shortcuts) -
-    func doLocate(with block: WKRPTCLGeoBlkString?)
+    func doLocate(with block: WKRPTCLGeoBlkStringLocation?)
+    func doLocate(_ address: DNSPostalAddress,
+                  with block: WKRPTCLGeoBlkStringLocation?)
     func doTrackLocation(for processKey: String,
-                         with block: WKRPTCLGeoBlkString?)
+                         with block: WKRPTCLGeoBlkStringLocation?)
 }
